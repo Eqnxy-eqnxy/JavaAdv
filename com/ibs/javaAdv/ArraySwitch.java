fcce5f316056f6ec2880d3433a0c104daf5feff7
Eqnxy <r.fadejev@gmail.com>
2023-08-22 23:33:26 +0300

    final ver

--- com/ibs/javaAdv/ArraySwitch.java
+++ com/ibs/javaAdv/ArraySwitch.java
@@ -3,28 +3,48 @@ package com.ibs.javaAdv;
 public class ArraySwitch {
     public static void main(String[] args) {
         int[] arr = new int[20];
-        int maxInd = 0;
-        int minInd = 0;
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = ((int) (Math.random() * 21) - 10);
-            System.out.println(arr[i]);
         }
 
-        for (int i = 1; i < arr.length - 1; i++) {
-            if (arr[i - 1] > arr[i]) {
-                maxInd = arr[i - 1];
-            }
-            if ((arr[i + 1] < 0) && (arr[i] > 0)) {
-                minInd = arr[i];
+        int maxNeg = Integer.MAX_VALUE;
+        int maxNegIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < maxNeg && arr[i] < 0) {
+                maxNeg = arr[i];
+                maxNegIndex = i;
             }
         }
 
-        int temp = arr[minInd];
-        arr[minInd] = arr[maxInd];
-        arr[maxInd] = temp;
+        int minPos = Integer.MAX_VALUE;
+        int minPosIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0 && arr[i] < minPos) {
+                minPos = arr[i];
+                minPosIndex = i;
+            }
+        }
 
-        System.out.println("min =" + minInd + " max = " + maxInd);
+        System.out.println("\nДо подмены:");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nmax negative = " + maxNeg + " index: " + maxNegIndex);
+        System.out.println("min positive = " + minPos + " index: " + minPosIndex);
 
+        if (maxNegIndex != -1 && minPosIndex != -1) {
+        int temp = arr[minPosIndex];
+        arr[minPosIndex] = arr[maxNegIndex];
+        arr[maxNegIndex] = temp;
+            } else {
+            System.out.println("Недостаточно значений в массиве для подмены");
+        }
+        System.out.println("\nПосле подмены:");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nmax negative = " + arr[maxNegIndex] + " index: " + maxNegIndex);
+        System.out.println("min positive = " + arr[minPosIndex] + " index: " + minPosIndex);
     }
 }
